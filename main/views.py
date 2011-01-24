22e99c2cf86909141aeec5b4f60a2753d124d5b5
Anders Pearson <anders@columbia.edu>
2011-01-24 14:53:10 -0500

    basic faceted browsing of videos

--- main/views.py
+++ main/views.py
@@ -11,6 +11,7 @@ import os
 from angeldust import PCP
 from django.conf import settings
 from django.db import transaction
+from django.core.paginator import Paginator, EmptyPage, InvalidPage
 
 class rendered_with(object):
     def __init__(self, template_name):
@@ -40,6 +41,43 @@ def series(request,id):
     return dict(series=series,videos=videos[:20],
                 operations=Operation.objects.filter(video__series__id=id).order_by("-modified")[:20])
 
+
+@login_required
+@rendered_with('main/video_index.html')
+def video_index(request):
+    videos = Video.objects.all()
+    creators = request.GET.getlist('creator')
+    if len(creators) > 0:
+        videos = videos.filter(creator__in=creators)
+    descriptions = request.GET.getlist('description')
+    if len(descriptions) > 0:
+        videos = videos.filter(description__in=descriptions)
+    languages = request.GET.getlist('language')
+    if len(languages) > 0:
+        videos = videos.filter(language__in=languages)
+    subjects = request.GET.getlist('subject')
+    if len(subjects) > 0:
+        videos = videos.filter(subject__in=subjects)
+    licenses = request.GET.getlist('license')
+    if len(licenses) > 0:
+        videos = videos.filter(license__in=licenses)
+    paginator = Paginator(videos.order_by('title'),100)
+    
+    try:
+        page = int(request.GET.get('page','1'))
+    except ValueError:
+        page = 1
+
+    try:
+        videos = paginator.page(page)
+    except (EmptyPage, InvalidPage):
+        videos = paginator.page(paginator.num_pages)
+    params = dict()
+    for k,v in request.GET.items():
+        params[k] = v
+    params.update(dict(videos=videos))
+    return params
+    
 
 @login_required
 @rendered_with('main/add_series.html')
