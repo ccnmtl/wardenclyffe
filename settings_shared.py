7fc776125a7d18be587f0cc17d6a13814c7d01d8
Anders Pearson <anders@columbia.edu>
2011-09-20 09:49:08 -0400

    put myself on the annoy list too till we figure out what is up

--- settings_shared.py
+++ settings_shared.py
@@ -100,7 +100,7 @@ EMAIL_SUBJECT_PREFIX = "[wardenclyffe] "
 EMAIL_HOST = 'localhost'
 SERVER_EMAIL = "wardenclyffe@ccnmtl.columbia.edu"
 # email addresses of video team members how want to be annoyed by lots of status email
-ANNOY_EMAILS = ["agraddy@columbia.edu",]
+ANNOY_EMAILS = ["agraddy@columbia.edu","anders@columbia.edu"]
 
 # WIND settings
 
