02d7e94e889cf3a06a4fa0c54e8687204b9c1d01
Anders Pearson <anders@columbia.edu>
2011-09-27 16:14:02 -0400

    Wind-auth MP4 testcase

--- surelink/tests.py
+++ surelink/tests.py
@@ -64,3 +64,35 @@ class PublicMP4TestCase(unittest.TestCase):
     def testMDPEmbed(self):
         self.assertEquals(self.surelink.mdp_embed(),
                           """[mp4]http://ccnmtl.columbia.edu/broadcast/test/test_clip.mp4[w]480[h]360[mp4]""")
+
+
+class WindMP4TestCase(unittest.TestCase):
+    def setUp(self):
+        self.surelink = SureLink("test/test_clip.mp4",
+                                 480,360,"",
+                                 "http://ccnmtl.columbia.edu/broadcast/posters/vidthumb_480x360.jpg",
+                                 "protected","wind","v4",
+                                 settings.SURELINK_PROTECTION_KEY)
+
+    def testProtection(self):
+        self.assertEquals(self.surelink.get_protection(),"18e74f6f8998963c72154f969f11d8f3ad91345d")
+
+    def testBasicEmbed(self):
+        self.assertEquals(self.surelink.basic_embed(),
+                          """<script type="text/javascript" src="http://ccnmtl.columbia.edu/stream/jsembed?player=v4&file=test/test_clip.mp4&width=480&height=360&poster=http://ccnmtl.columbia.edu/broadcast/posters/vidthumb_480x360.jpg&authtype=wind"></script>""")
+
+    def testIFrameEmbed(self):
+        self.assertEquals(self.surelink.iframe_embed(),
+                          """<iframe width="480" height="384" src="https://surelink.ccnmtl.columbia.edu/video/?player=v4&file=test/test_clip.mp4&width=480&height=360&poster=http://ccnmtl.columbia.edu/broadcast/posters/vidthumb_480x360.jpg&authtype=wind" />""")
+
+    def testEdblogsEmbed(self):
+        self.assertEquals(self.surelink.edblogs_embed(),
+                          """[ccnmtl_video src="http://ccnmtl.columbia.edu/stream/jsembed?player=v4&file=test/test_clip.mp4&width=480&height=360&poster=http://ccnmtl.columbia.edu/broadcast/posters/vidthumb_480x360.jpg&authtype=wind"]""")
+        
+    def testDrupalEmbed(self):
+        self.assertEquals(self.surelink.drupal_embed(),
+                          """http://ccnmtl.columbia.edu/stream/flv/xdrupalx/OPTIONS/test/test_clip.mp4""")
+
+    def testMDPEmbed(self):
+        self.assertEquals(self.surelink.mdp_embed(),
+                          """[flv]http://ccnmtl.columbia.edu/stream/flv/5916f0fe8ab583c47adf39fbe3a80086b7122994/OPTIONS/test/test_clip.mp4[w]480[h]360[flv]""")
