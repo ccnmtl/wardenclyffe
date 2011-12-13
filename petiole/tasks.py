d2b4a2ef6391a27ce40aa806737f93d8cde684c4
Anders Pearson <anders@columbia.edu>
2011-12-13 17:15:08 -0500

    petiole is the future direction of the project. abstraction of job dependency handling

---
+++ petiole/tasks.py
@@ -0,0 +1,13 @@
+from celery.decorators import task
+import petiole.models
+
+@task
+def run_jobset(jobset_id):
+    js = petiole.models.JobSet.objects.get(id=jobset_id)
+    js.run()
+
+@task
+def run_job(job_id):
+    j = petiole.models.Job.objects.get(id=job_id)
+    j.run()
+
