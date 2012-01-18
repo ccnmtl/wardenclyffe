71657eb475f400b83020129ff5c22c27425ab5ba
Anders Pearson <anders@columbia.edu>
2012-01-18 11:44:28 -0500

    filling out unit tests a bit

--- main/tests.py
+++ main/tests.py
@@ -25,6 +25,9 @@ class CUITFileTest(TestCase):
     def test_is_cuit(self):
         assert self.file.is_cuit()
 
+    def test_surelinkable(self):
+        assert self.file.surelinkable()
+
     def test_tahoe_download_url(self):
         assert self.file.tahoe_download_url() is None
 
@@ -32,6 +35,7 @@ class CUITFileTest(TestCase):
         assert self.video.mediathread_url() == "http://ccnmtl.columbia.edu/stream/flv/b16c80a7e6b21e3671f8f7fa4ec468777f7e1e8b/OPTIONS/secure/courses/56d27944-4131-11e1-8164-0017f20ea192-Mediathread_video_uploaded_by_mlp55.flv"
 
     def test_poster_url(self):
+        assert self.file.has_cuit_poster() == False
         assert self.video.cuit_poster_url() is None
 
 
