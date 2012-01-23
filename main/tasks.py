a96031e33a65858be77ffef00a02ffc8c4abebaf
Anders Pearson <anders@columbia.edu>
2012-01-23 13:46:07 -0500

    that was moved to the vital app

--- main/tasks.py
+++ main/tasks.py
@@ -54,65 +54,6 @@ def save_file_to_tahoe(operation,params):
     of = OperationFile.objects.create(operation=operation,file=f)
     return ("complete","")
 
-@task(ignore_result=True)
-def submit_to_vital(video_id,user,course_id,rtsp_url,vital_secret,vital_base,**kwargs):
-    print "submitting to vital"
-    video = Video.objects.get(id=video_id)
-
-    action = "submit to vital"
-    def _do_submit_to_vital(video,user,operation,course_id,rtsp_url,vital_secret,vital_base,**kwargs):
-        (width,height) = video.get_dimensions()
-        if not width or not height:
-            return ("failed","could not figure out dimensions")
-        if not rtsp_url:
-            return ("failed","no video URL")
-        params = {
-            'set_course' : course_id,
-            'as' : user.username,
-            'secret' : vital_secret,
-            'title' : video.title,
-            'url' : rtsp_url,
-            'thumb' : video.vital_thumb_url(),
-            }
-        resp,content = POST(vital_base,params=params,async=False,resp=True)
-        if resp.status == 302 or resp.status == 200:
-            send_mail('VITAL video uploaded', 
-                  """Your video, "%s", has been uploaded to VITAL.""" % video.title, 
-                  'wardenclyffe@wardenclyffe.ccnmtl.columbia.edu',
-                  ["%s@columbia.edu" % user.username], fail_silently=False)
-            # annoy the video team
-            for vuser in settings.ANNOY_EMAILS:
-                send_mail('VITAL video uploaded', 
-                          """Your video, "%s", has been uploaded to VITAL.""" % video.title, 
-                          'wardenclyffe@wardenclyffe.ccnmtl.columbia.edu',
-                          [vuser], fail_silently=False)
-            return ("complete","")
-        else:
-            send_mail('VITAL video upload failed', 
-                  """An error has occurred while attempting to upload your video, "%s", to VITAL.
-Please contact CCNMTL video staff for assistance. 
-The error encountered:
-%s
-""" % (video.title,content), 
-                  'wardenclyffe@wardenclyffe.ccnmtl.columbia.edu',
-                  ["%s@columbia.edu" % user.username], fail_silently=False)
-            for vuser in settings.ANNOY_EMAILS:
-                send_mail('VITAL video upload failed', 
-                          """An error has occurred while attempting to upload your video, "%s", to VITAL.
-Please contact CCNMTL video staff for assistance. 
-The error encountered:
-%s
-""" % (video.title,content), 
-                          'wardenclyffe@wardenclyffe.ccnmtl.columbia.edu',
-                          [vuser], fail_silently=False)
-
-            return ("failed","vital rejected submission: %s" % content)
-    args = [course_id,rtsp_url,vital_secret,vital_base]
-    with_operation(_do_submit_to_vital,video,
-                   "submit to vital","",
-                   user,args,kwargs)
-    print "done submitting to vital"
-
 def make_images(operation,params):
     ouuid = operation.uuid
     tmpfilename = params['tmpfilename']
