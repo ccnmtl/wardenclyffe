773604425c96c05f1c6776f3a82a9c402373f667
Anders Pearson <anders@columbia.edu>
2012-06-04 15:26:37 -0400

    flake8 compliance

--- vital/urls.py
+++ vital/urls.py
@@ -1,10 +1,12 @@
 from django.conf.urls.defaults import patterns
 
 urlpatterns = patterns('vital.views',
-                       (r'^done/$','done',{},'vital-done'),
-                       (r'^received/$','received',{},'vital-received'),
-                       (r'^posterdone/$','posterdone',{},'vital-posterdone'),
-                       (r'^drop/$','drop_form',{},'vital-drop-form'),
-                       (r'^drop/post/$','drop',{},'vital-drop'),
-                       (r'^submit/(?P<id>\d+)/$','submit',{},'vital-submit'),
+                       (r'^done/$', 'done', {}, 'vital-done'),
+                       (r'^received/$', 'received', {}, 'vital-received'),
+                       (r'^posterdone/$', 'posterdone', {},
+                        'vital-posterdone'),
+                       (r'^drop/$', 'drop_form', {}, 'vital-drop-form'),
+                       (r'^drop/post/$', 'drop', {}, 'vital-drop'),
+                       (r'^submit/(?P<id>\d+)/$', 'submit', {},
+                        'vital-submit'),
 )
--- vital/views.py
+++ vital/views.py
@@ -1,9 +1,9 @@
 # Create your views here.
 from annoying.decorators import render_to
-from django.contrib.auth.decorators import login_required
 from django.shortcuts import get_object_or_404
 from django.http import HttpResponse, HttpResponseRedirect
-from wardenclyffe.main.models import Video, Operation, Collection, File, Metadata, OperationLog, OperationFile, Image, Poster
+from wardenclyffe.main.models import Video, Operation, Collection, File
+from wardenclyffe.main.models import OperationLog
 from django.contrib.auth.models import User
 import wardenclyffe.vital.tasks as tasks
 import wardenclyffe.main.tasks as maintasks
@@ -11,15 +11,17 @@ from wardenclyffe.util import uuidparse
 from django.conf import settings
 from django.db import transaction
 import uuid
-import hmac, hashlib, datetime
-from django.template import RequestContext
+import hmac
+import hashlib
 from django_statsd.clients import statsd
 from wardenclyffe.util.mail import send_vital_received_mail
+import os
+
 
 @transaction.commit_manually
 @render_to('vital/submit.html')
-def submit(request,id):
-    v = get_object_or_404(Video,id=id)
+def submit(request, id):
+    v = get_object_or_404(Video, id=id)
     if request.method == "POST":
         statsd.incr("vital.submit")
         # make db entry
@@ -27,36 +29,41 @@ def submit(request,id):
             # we make a "vitalsubmit" file to store the submission
             # info and serve as a flag that it needs to be submitted
             # (when PCP comes back)
-            if not request.POST.get('bypass',False):
+            if not request.POST.get('bypass', False):
                 submit_file = File.objects.create(video=v,
                                                   label="vital submit",
                                                   location_type='vitalsubmit')
-                submit_file.set_metadata("username",request.user.username)
-                submit_file.set_metadata("set_course",request.POST['course_id'])
-                submit_file.set_metadata("notify_url",settings.VITAL_NOTIFY_URL)
+                submit_file.set_metadata("username",
+                                         request.user.username)
+                submit_file.set_metadata("set_course",
+                                         request.POST['course_id'])
+                submit_file.set_metadata("notify_url",
+                                         settings.VITAL_NOTIFY_URL)
         except:
             statsd.incr("vital.submit.failure")
             transaction.rollback()
             raise
         else:
             transaction.commit()
-            if request.POST.get('bypass',False):
-                vt = File.objects.filter(video=v,location_type='vitalthumb')
-                qt = File.objects.filter(video=v,location_type='rtsp_url')
-                
-                tasks.submit_to_vital.delay(v.id,request.user,
+            if request.POST.get('bypass', False):
+                vt = File.objects.filter(video=v, location_type='vitalthumb')
+                qt = File.objects.filter(video=v, location_type='rtsp_url')
+
+                tasks.submit_to_vital.delay(v.id, request.user,
                                             request.POST['course_id'],
                                             qt[0].url,
                                             settings.VITAL_SECRET,
                                             settings.VITAL_NOTIFY_URL)
             else:
                 workflow = settings.PCP_WORKFLOW
-                if hasattr(settings,'VITAL_PCP_WORKFLOW'):
+                if hasattr(settings, 'VITAL_PCP_WORKFLOW'):
                     workflow = settings.VITAL_PCP_WORKFLOW
-                maintasks.pull_from_tahoe_and_submit_to_pcp.delay(v.id,request.user,workflow,settings.PCP_BASE_URL,settings.PCP_USERNAME,settings.PCP_PASSWORD)
+                maintasks.pull_from_tahoe_and_submit_to_pcp.delay(
+                    v.id, request.user, workflow, settings.PCP_BASE_URL,
+                    settings.PCP_USERNAME, settings.PCP_PASSWORD)
             return HttpResponseRedirect(v.get_absolute_url())
-    vt = File.objects.filter(video=v,location_type='vitalthumb')
-    qt = File.objects.filter(video=v,location_type='rtsp_url')
+    vt = File.objects.filter(video=v, location_type='vitalthumb')
+    qt = File.objects.filter(video=v, location_type='rtsp_url')
     return dict(video=v,
                 bypassable=vt.count() > 0 and qt.count() > 0)
 
@@ -70,83 +77,88 @@ def drop(request):
             statsd.incr("vital.drop")
             # save it locally
             vuuid = uuid.uuid4()
-            try: 
+            try:
                 os.makedirs(settings.TMP_DIR)
             except:
                 pass
             extension = request.FILES['source_file'].name.split(".")[-1]
-            tmpfilename = settings.TMP_DIR + "/" + str(vuuid) + "." + extension.lower()
+            tmpfilename = (settings.TMP_DIR + "/" + str(vuuid) + "." +
+                           extension.lower())
             tmpfile = open(tmpfilename, 'wb')
             for chunk in request.FILES['source_file'].chunks():
                 tmpfile.write(chunk)
             tmpfile.close()
             # make db entry
             try:
-                collection = Collection.objects.get(id=settings.VITAL_COLLECTION_ID)
+                collection = Collection.objects.get(
+                    id=settings.VITAL_COLLECTION_ID)
                 filename = request.FILES['source_file'].name
-                user = User.objects.get(username=request.session['username'])                
+                user = User.objects.get(username=request.session['username'])
                 v = Video.objects.create(collection=collection,
-                                         title=request.POST.get('title',''),
+                                         title=request.POST.get('title', ''),
                                          creator=request.session['username'],
-                                         uuid = vuuid)
+                                         uuid=vuuid)
                 source_file = File.objects.create(video=v,
                                                   label="source file",
-                                                  filename=request.FILES['source_file'].name,
+                                                  filename=filename,
                                                   location_type='none')
                 # we make a "vitalsubmit" file to store the submission
                 # info and serve as a flag that it needs to be submitted
                 # (when PCP comes back)
                 submit_file = File.objects.create(video=v,
                                                   label="vital submit",
-                                                  filename=request.FILES['source_file'].name,
+                                                  filename=filename,
                                                   location_type='vitalsubmit')
-                submit_file.set_metadata("username",request.session['username'])
-                submit_file.set_metadata("set_course",request.session['set_course'])
-                submit_file.set_metadata("redirect_to",request.session['redirect_to'])
-                submit_file.set_metadata("notify_url",request.session['notify_url'])
+                submit_file.set_metadata("username",
+                                         request.session['username'])
+                submit_file.set_metadata("set_course",
+                                         request.session['set_course'])
+                submit_file.set_metadata("redirect_to",
+                                         request.session['redirect_to'])
+                submit_file.set_metadata("notify_url",
+                                         request.session['notify_url'])
 
-                params = dict(tmpfilename=tmpfilename,source_file_id=source_file.id)
-                o = Operation.objects.create(uuid = uuid.uuid4(),
+                params = dict(tmpfilename=tmpfilename,
+                              source_file_id=source_file.id)
+                o = Operation.objects.create(uuid=uuid.uuid4(),
                                              video=v,
                                              action="extract metadata",
                                              status="enqueued",
                                              params=params,
                                              owner=user)
-                operations.append((o.id,params))
-                params = dict(tmpfilename=tmpfilename,filename=tmpfilename,
+                operations.append((o.id, params))
+                params = dict(tmpfilename=tmpfilename, filename=tmpfilename,
                               tahoe_base=settings.TAHOE_BASE)
-                o = Operation.objects.create(uuid = uuid.uuid4(),
+                o = Operation.objects.create(uuid=uuid.uuid4(),
                                              video=v,
                                              action="save file to tahoe",
                                              status="enqueued",
                                              params=params,
                                              owner=user
                                              )
-                operations.append((o.id,params))
+                operations.append((o.id, params))
                 params = dict(tmpfilename=tmpfilename)
-                o = Operation.objects.create(uuid = uuid.uuid4(),
+                o = Operation.objects.create(uuid=uuid.uuid4(),
                                              video=v,
                                              action="make images",
                                              status="enqueued",
                                              params=params,
-                                             owner=user
-                                             )
-                operations.append((o.id,params))
-                
+                                             owner=user)
+                operations.append((o.id, params))
                 workflow = settings.PCP_WORKFLOW
-                if hasattr(settings,'VITAL_PCP_WORKFLOW'):
+                if hasattr(settings, 'VITAL_PCP_WORKFLOW'):
                     workflow = settings.VITAL_PCP_WORKFLOW
                     params = dict(tmpfilename=tmpfilename,
                                   pcp_workflow=workflow)
 
-                    o = Operation.objects.create(uuid = uuid.uuid4(),
-                                                 video=v,
-                                                 action="submit to podcast producer",
-                                                 status="enqueued",
-                                                 params=params,
-                                                 owner=user
-                                                 )
-                    operations.append((o.id,params))
+                    o = Operation.objects.create(
+                        uuid=uuid.uuid4(),
+                        video=v,
+                        action="submit to podcast producer",
+                        status="enqueued",
+                        params=params,
+                        owner=user)
+                    operations.append((o.id, params))
             except:
                 statsd.incr("vital.drop.failure")
                 transaction.rollback()
@@ -154,21 +166,23 @@ def drop(request):
             else:
                 transaction.commit()
 
-                for o,kwargs in operations:
-                    maintasks.process_operation.delay(o,kwargs)
+                for o, kwargs in operations:
+                    maintasks.process_operation.delay(o, kwargs)
                 return HttpResponseRedirect(request.session['redirect_to'])
+
 
 @render_to('vital/drop.html')
 def drop_form(request):
     # check their credentials
-    nonce = request.GET.get('nonce','')
-    hmc = request.GET.get('hmac','')
-    set_course = request.GET.get('set_course','')
+    nonce = request.GET.get('nonce', '')
+    hmc = request.GET.get('hmac', '')
+    set_course = request.GET.get('set_course', '')
     username = request.GET.get('as')
-    redirect_to = request.GET.get('redirect_url','')
-    notify_url = request.GET.get('notify_url','')
+    redirect_to = request.GET.get('redirect_url', '')
+    notify_url = request.GET.get('notify_url', '')
     verify = hmac.new(settings.VITAL_SECRET,
-                      '%s:%s:%s:%s' % (username,redirect_to,notify_url,nonce),
+                      '%s:%s:%s:%s' % (username, redirect_to, notify_url,
+                                       nonce),
                       hashlib.sha1
                       ).hexdigest()
     if verify != hmc:
@@ -193,10 +207,11 @@ def drop_form(request):
 
     return dict(user=user)
 
+
 def done(request):
     if 'title' not in request.POST:
         return HttpResponse("expecting a title")
-    title = request.POST.get('title','no title')
+    title = request.POST.get('title', 'no title')
     uuid = uuidparse(title)
     r = Operation.objects.filter(uuid=uuid)
     if r.count() == 1:
@@ -204,56 +219,62 @@ def done(request):
         operation = r[0]
         operation.status = "complete"
         operation.save()
-        ol = OperationLog.objects.create(operation=operation,
-                                         info="PCP completed")
+        OperationLog.objects.create(operation=operation,
+                                    info="PCP completed")
         if operation.video.is_vital_submit():
-            cunix_path = request.POST.get('movie_destination_path','')
-            rtsp_url = cunix_path.replace("/media/qtstreams/projects/","rtsp://qtss.cc.columbia.edu/projects/")
-            (set_course,username,notify_url) = operation.video.vital_submit()
+            cunix_path = request.POST.get('movie_destination_path', '')
+            rtsp_url = cunix_path.replace(
+                "/media/qtstreams/projects/",
+                "rtsp://qtss.cc.columbia.edu/projects/")
+            (set_course, username, notify_url) = operation.video.vital_submit()
             if set_course is not None:
                 user = User.objects.get(username=username)
-                qtf = File.objects.create(video=operation.video,
-                                          label="Quicktime Streaming Video",
-                                          url=rtsp_url,
-                                          location_type='rtsp_url')
-                tasks.submit_to_vital.delay(operation.video.id,user,set_course,
+                File.objects.create(video=operation.video,
+                                    label="Quicktime Streaming Video",
+                                    url=rtsp_url,
+                                    location_type='rtsp_url')
+                tasks.submit_to_vital.delay(operation.video.id, user,
+                                            set_course,
                                             rtsp_url,
                                             settings.VITAL_SECRET,
                                             notify_url)
                 operation.video.clear_vital_submit()
     return HttpResponse("ok")
 
+
 def posterdone(request):
     if 'title' not in request.POST:
         return HttpResponse("expecting a title")
-    title = request.POST.get('title','no title')
+    title = request.POST.get('title', 'no title')
     uuid = uuidparse(title)
     r = Operation.objects.filter(uuid=uuid)
     if r.count() == 1:
         statsd.incr("vital.posterdone")
         operation = r[0]
         if operation.video.is_vital_submit():
-            cunix_path = request.POST.get('image_destination_path','')
-            poster_url = cunix_path.replace("/www/data/ccnmtl/broadcast/projects/vital/thumbs/",
-                                            "http://ccnmtl.columbia.edu/broadcast/projects/vital/thumbs/")
+            cunix_path = request.POST.get('image_destination_path', '')
+            poster_url = cunix_path.replace(
+                "/www/data/ccnmtl/broadcast/projects/vital/thumbs/",
+                "http://ccnmtl.columbia.edu/broadcast/projects/vital/thumbs/")
 
-            vitalthumb_file = File.objects.create(video=operation.video,
-                                                  label="vital thumbnail image",
-                                                  url=poster_url,
-                                                  location_type='vitalthumb')
+            File.objects.create(video=operation.video,
+                                label="vital thumbnail image",
+                                url=poster_url,
+                                location_type='vitalthumb')
     return HttpResponse("ok")
+
 
 def received(request):
     if 'title' not in request.POST:
         return HttpResponse("expecting a title")
-    title = request.POST.get('title','no title')
+    title = request.POST.get('title', 'no title')
     uuid = uuidparse(title)
     r = Operation.objects.filter(uuid=uuid)
     if r.count() == 1:
         statsd.incr("vital.received")
         operation = r[0]
         if operation.video.is_vital_submit():
-            send_vital_received_mail(operation.video.title, 
+            send_vital_received_mail(operation.video.title,
                                      operation.owner.username)
 
     return HttpResponse("ok")
