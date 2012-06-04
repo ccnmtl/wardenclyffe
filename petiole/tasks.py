c2f958180b2a6053dde3854ff116178a6e11265d
Anders Pearson <anders@columbia.edu>
2012-06-04 14:39:09 -0400

    flake8 compliance

--- petiole/tasks.py
+++ petiole/tasks.py
@@ -1,13 +1,14 @@
 from celery.decorators import task
 import petiole.models
 
+
 @task
 def run_jobset(jobset_id):
     js = petiole.models.JobSet.objects.get(id=jobset_id)
     js.run()
 
+
 @task
 def run_job(job_id):
     j = petiole.models.Job.objects.get(id=job_id)
     j.run()
-
