1243f22861c648274c649cdb62efa78a0e730ca0
Anders Pearson <anders@columbia.edu>
2012-08-29 16:57:14 -0400

    convert operations dashboard javascript to backbone

--- media/js/dashboard.js
+++ media/js/dashboard.js
@@ -1,241 +1,208 @@
-var Operation = Backbone.Model.extend({
-    defaults: {
-    },
-    initialize: function () {
-    }
-});
+$(document).ready(
+    function () {
 
-var Operations = Backbone.Collection.extend({
-    model: Operation
-});
-
-var buildOperationView = function (operationModel, operationController) {
-    var base = document.createElement('div'),
-    operationEl = document.createElement('div');
-
-    base.appendChild(operationEl);
-
-    var render = function () {
-        // We use a templating library such as Underscore
-        // templating which generates the HTML for our
-        // photo entry
-        operationEl.innerHTML = _.template('operationTemplate', {});
-    };
-
-    operationModel.addSubscriber(render);
-
-    operationEl.addEventListener('click', function () {
-        operationController.handleEvent('click', operationModel);
-    });
-
-    var show = function () {
-        operationEl.style.display  = '';
-    };
-
-    var hide = function () {
-        operationEl.style.display  = 'none';
-    };
-    return {
-        showView: show,
-        hideView: hide
-    };
-};
-
-function getQueryParams() {
-    var vars = {}, hash;
-    var hashes = window.location.href.slice(window.location.href.indexOf('?') + 1).split('&');
-    for (var i = 0; i < hashes.length; i++) {
-        hash = hashes[i].split('=');
-        vars[hash[0]] = hash[1];
-    }
-    return vars;
-}
-
-var cleanStatus = function (s) {
-    return s.replace(" ", "");
-};
-
-var newRow = function (el) {
-    var r = $("<tr></tr>");
-    r.attr("id", "operation_" + el.id);
-    r.append($("<td></td>")
-             .append($("<div class=\"operation_status\"></div>").addClass(cleanStatus(el.status))));
-    r.append("<td>" + el.action + "</td>");
-
-    r.append($("<td></td>")
-             .append($("<a href='" + el.video_url + "'>" + el.video_title + "</a>")));
-
-    r.append($("<td></td>")
-             .append($("<a href='" + el.collection_url + "'>" + el.collection_title + "</a>")));
-
-    r.append("<td class=\"modified\">" + el.modified + "</td>");
-    r.append("<td>" + el.video_creator + "</td>");
-
-    return r;
-};
-
-var WCgetRow = function (operation_id) {
-    var r = $("#operation_" + operation_id);
-    return r;
-};
-
-var updateRow = function (oldRow, el) {
-    // only fields likely to change are status and date
-    var row = $("#operation_" + el.id);
-    row.children(".modified").text(el.modified);
-    $(".operation_status", row)
-        .removeClass("complete")
-        .removeClass("failed")
-        .removeClass("inprogress")
-        .removeClass("submitted")
-        .addClass(cleanStatus(el.status));
-};
-
-var orderTableByDate = function () {
-    $("#operations").trigger("update");
-};
-
-var stripeTable = function () {
-    $(".even").removeClass("even");
-    $(".odd").removeClass("odd");
-    $("#operations tbody tr:odd").addClass("odd");
-    $("#operations tbody tr:even").addClass("even");
-};
-
-var maxRows = 200;
-
-var trimTable = function (maxRows) {
-};
-
-var sortInitialized = 0;
-var mostRecentOperation = "";
-
-var defaultRefresh = 10000; // 10 seconds
-var maxRefresh = 1000 * 5 * 60; // 5 minutes
-var currentRefresh = defaultRefresh;
-
-var WCRefresh = function (e) {
-    // first, we check if there are new operations at all
-    // by calling /num_operations/ and comparing.
-    $.ajax({
-        url: "/most_recent_operation/",
-        type: "get",
-        dateType: 'json',
-        error: requestFailed,
-        success: getMostRecentSuccess
-    });
-};
-
-var getMostRecentSuccess = function (d) {
-    if (!d) {
-        requestFailed();
-        return;
-    }
-    if (d.modified !== mostRecentOperation) {
-        mostRecentOperation = d.modified;
-        var data = getQueryParams();
-        refreshOperations(data);
-    }
-    currentRefresh = defaultRefresh;
-    setTimeout(WCRefresh, defaultRefresh);
-};
-
-var requestFailed = function () {
-    // ease up on the server when it's having trouble
-    currentRefresh = 2 * currentRefresh; // double the refresh time
-    if (currentRefresh > maxRefresh) {
-        currentRefresh = maxRefresh;
-    }
-    setTimeout(WCRefresh, currentRefresh);
-};
-
-var addOperation = function (o) {
-    var r = newRow(o);
-    $("#operations tbody").prepend(r);
-};
-
-var refreshOperationsSuccess = function (d) {
-    if (!d) {
-        requestFailed();
-        return;
-    }
-    if (d.operations.length) {
-        var rowsToAdd    = [];
-        var rowsToUpdate = [];
-        var rowsToDelete = [];
-        
-        for (var i = d.operations.length - 1; i >= 0; i--) {
-            // TODO: compare/update
-            var el = d.operations[i];
-            var operation_id = el.id;
-            var oldRow = WCgetRow(operation_id);
-            if (oldRow.length > 0) {
-                rowsToUpdate.push([oldRow, el]);
-            } else {
-                rowsToAdd.push(el);
+        var Operation = Backbone.Model.extend({
+            defaults: {
+            },
+            initialize: function () {
+                this.set({clean_status: cleanStatus(this.get("status"))});
+                this.on('change:status', function () {
+                    this.set({clean_status: cleanStatus(this.get("status"))});
+                });
             }
-        }
-        if (rowsToUpdate.length > 0) {
-            for (i = 0; i < rowsToUpdate.length; i++) {
-                updateRow(rowsToUpdate[i][0], rowsToUpdate[i][1]);
-            }
-        }
-        if (rowsToAdd.length > 0) {
-            for (i = 0; i < rowsToAdd.length; i++) {
-                addOperation(rowsToAdd[i]);
-            }
-        }
-        if (sortInitialized === 0) {
-            $("#operations").tablesorter({sortList: [[4, 1]]});
-            sortInitialized = 1;
-        }
-        orderTableByDate();
-        stripeTable();
-        trimTable(maxRows);
-    }
-};
-
-
-var refreshOperations = function (data) {
-		$.ajax({
-            url: "/recent_operations/",
-            type: 'get',
-            dataType: 'json',
-            data: data,
-            error: requestFailed,
-            success: refreshOperationsSuccess
         });
-    };
 
-
-jQuery(function ($) {
-    // Ensure that the CSRF token is sent with AJAX POSTs sent by jQuery
-    // Taken from the documentation: http://docs.djangoproject.com/en/dev/ref/contrib/csrf/
-    $('html').ajaxSend(function (event, xhr, settings) {
-        function getCookie(name) {
-            var cookieValue = null;
-            if (document.cookie && document.cookie !== '') {
-                var cookies = document.cookie.split(';');
-                for (var i = 0; i < cookies.length; i++) {
-                    var cookie = jQuery.trim(cookies[i]);
-                    // Does this cookie string begin with the name we want?
-                    if (cookie.substring(0, name.length + 1) === (name + '=')) {
-                        cookieValue = decodeURIComponent(cookie.substring(name.length + 1));
-                        break;
-                    }
+        var Operations = Backbone.Collection.extend({
+            model: Operation,
+            url: "/foo", // have to set a URL or it complains
+            addOrUpdateOperation: function (o) {
+                var op = this.get(o.id);
+                if (op !== undefined) {
+                    op.set(o);
+                } else {
+                    this.add(o);
                 }
             }
-            return cookieValue;
-        }
-        if (!(/^http:.*/.test(settings.url) || (/^https:.*/.test(settings.url)))) {
-            // Only send the token to relative URLs i.e. locally.
-            xhr.setRequestHeader("X-CSRFToken", getCookie('csrftoken'));
-        }
-    });
+        });
 
-    $(document).ready(
-        function () {
-            WCRefresh();
+        Backbone.sync = function (method, model, options) {
+            // dummy this out since we never actually
+            // want to sync back to the server
+        };
+
+        var AllOperations = new Operations();
+        
+        var OperationView = Backbone.View.extend({
+            tagName: "tr",
+            template: _.template($('#operation-template').html()),
+            events: {},
+            initialize: function () {
+                _.bindAll(this, 'render');
+                this.model.on('change', this.render);
+                this.model.on('destroy', this.remove);
+            },
+            render: function () {
+                this.$el.html(this.template(this.model.toJSON()));
+                return this;
+            }
+        });
+
+        var AppView = Backbone.View.extend({
+            el: $('#bb-operations'),
+            initialize: function () {
+                AllOperations.bind('add', this.addOne, this);
+                AllOperations.bind('reset', this.addAll, this);
+                AllOperations.bind('all', this.render, this);
+                // AllOperations.fetch();
+            },
+            addOne: function (operation) {
+                var view = new OperationView({model: operation});
+                var e = view.render().el;
+                this.$el.prepend(e);
+            },
+            addAll: function () {
+                AllOperations.each(this.addOne);
+            },
+            render: function () {
+            }
+        });
+
+        var App = new AppView();
+
+        function getQueryParams() {
+            var vars = {}, hash;
+            var hashes = window.location.href.slice(window.location.href.indexOf('?') + 1).split('&');
+            for (var i = 0; i < hashes.length; i++) {
+                hash = hashes[i].split('=');
+                vars[hash[0]] = hash[1];
+            }
+            return vars;
         }
-    );
-});
+
+        var cleanStatus = function (s) {
+            return s.replace(" ", "");
+        };
+
+        var orderTableByDate = function () {
+            // jquery.tablesorter is responsible for table sorting
+            // just tell it it needs to update
+            $("#operations").trigger("update");
+        };
+
+        var stripeTable = function () {
+            $(".even").removeClass("even");
+            $(".odd").removeClass("odd");
+            $("#bb-operations tr:odd").addClass("odd");
+            $("#bb-operations tr:even").addClass("even");
+        };
+
+        var maxRows = 200;
+
+        var trimTable = function (maxRows) {
+            // todo: just trim AllOperations to maxRows
+        };
+
+        var sortInitialized = 0;
+        var mostRecentOperation = "";
+
+        var defaultRefresh = 10000; // 10 seconds
+        var maxRefresh = 1000 * 5 * 60; // 5 minutes
+        var currentRefresh = defaultRefresh;
+
+        var WCRefresh = function (e) {
+            // first, we check if there are new operations at all
+            // by calling /num_operations/ and comparing.
+            $.ajax({
+                url: "/most_recent_operation/",
+                type: "get",
+                dateType: 'json',
+                error: requestFailed,
+                success: getMostRecentSuccess
+            });
+        };
+
+        var getMostRecentSuccess = function (d) {
+            if (!d) {
+                requestFailed();
+                return;
+            }
+            if (d.modified !== mostRecentOperation) {
+                mostRecentOperation = d.modified;
+                var data = getQueryParams();
+                refreshOperations(data);
+            }
+            currentRefresh = defaultRefresh;
+            setTimeout(WCRefresh, defaultRefresh);
+        };
+
+        var requestFailed = function () {
+            // ease up on the server when it's having trouble
+            currentRefresh = 2 * currentRefresh; // double the refresh time
+            if (currentRefresh > maxRefresh) {
+                currentRefresh = maxRefresh;
+            }
+            setTimeout(WCRefresh, currentRefresh);
+        };
+
+        var refreshOperationsSuccess = function (d) {
+            if (!d) {
+                requestFailed();
+                return;
+            }
+            if (d.operations.length) {
+                _.each(d.operations, AllOperations.addOrUpdateOperation, AllOperations);
+                if (sortInitialized === 0) {
+                    $("#operations").tablesorter({sortList: [[4, 1]]});
+                    sortInitialized = 1;
+                }
+                orderTableByDate();
+                stripeTable();
+                trimTable(maxRows);
+            }
+        };
+
+
+        var refreshOperations = function (data) {
+            $.ajax({
+                url: "/recent_operations/",
+                type: 'get',
+                dataType: 'json',
+                data: data,
+                error: requestFailed,
+                success: refreshOperationsSuccess
+            });
+        };
+
+
+        jQuery(function ($) {
+            // Ensure that the CSRF token is sent with AJAX POSTs sent by jQuery
+            // Taken from the documentation: http://docs.djangoproject.com/en/dev/ref/contrib/csrf/
+            $('html').ajaxSend(function (event, xhr, settings) {
+                function getCookie(name) {
+                    var cookieValue = null;
+                    if (document.cookie && document.cookie !== '') {
+                        var cookies = document.cookie.split(';');
+                        for (var i = 0; i < cookies.length; i++) {
+                            var cookie = jQuery.trim(cookies[i]);
+                            // Does this cookie string begin with the name we want?
+                            if (cookie.substring(0, name.length + 1) === (name + '=')) {
+                                cookieValue = decodeURIComponent(cookie.substring(name.length + 1));
+                                break;
+                            }
+                        }
+                    }
+                    return cookieValue;
+                }
+                if (!(/^http:.*/.test(settings.url) || (/^https:.*/.test(settings.url)))) {
+                    // Only send the token to relative URLs i.e. locally.
+                    xhr.setRequestHeader("X-CSRFToken", getCookie('csrftoken'));
+                }
+            });
+
+            $(document).ready(
+                function () {
+                    WCRefresh();
+                }
+            );
+        });
+    });
