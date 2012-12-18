42b05d399c9d5cab1cb4758a69e22619ce601314
Anders Pearson <anders@columbia.edu>
2012-12-18 11:16:34 -0500

    just list top level files instead of recursively (which is way too slow)

--- cuit/smoke.py
+++ cuit/smoke.py
@@ -1,9 +1,19 @@
 from smoketest import SmokeTest
-from wardenclyffe.cuit.views import list_all_cuit_files
+from django.conf import settings
+import paramiko
 
 
 class CUITSFTPTest(SmokeTest):
     def test_connectivity(self):
         """ this will be a good candidate for the @slow decorator
         once that is implemented """
-        self.assertTrue(len(list_all_cuit_files()) > 0)
+        sftp_hostname = settings.SFTP_HOSTNAME
+        sftp_path = settings.SFTP_PATH
+        sftp_user = settings.SFTP_USER
+        sftp_private_key_path = settings.SSH_PRIVATE_KEY_PATH
+        mykey = paramiko.RSAKey.from_private_key_file(sftp_private_key_path)
+        transport = paramiko.Transport((sftp_hostname, 22))
+        transport.connect(username=sftp_user, pkey=mykey)
+        sftp = paramiko.SFTPClient.from_transport(transport)
+        sftp.chdir(sftp_path)
+        self.assertTrue(len(sftp.listdir_attr()))
