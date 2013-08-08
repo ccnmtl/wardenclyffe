a33f3cd8018776b37a85a8bf41a92cb505166a5d
Anders Pearson <anders@columbia.edu>
2013-08-08 15:12:39 -0400

    operation count by status

--- wardenclyffe/graphite/models.py
+++ wardenclyffe/graphite/models.py
@@ -1,7 +1,15 @@
+from wardenclyffe.main.models import Operation
+
+
 def operation_count_by_status():
     """ get a count of all operations grouped by status
     -> dict() of {status -> int}
 
     failed  complete  submitted  in progress  enqueued
     """
-    return dict()
+    return dict(
+        failed=Operation.objects.filter(status="failed").count(),
+        complete=Operation.objects.filter(status="complete").count(),
+        submitted=Operation.objects.filter(status="submitted").count(),
+        inprogress=Operation.objects.filter(status="inprogress").count(),
+        enqueued=Operation.objects.filter(status="enqueued").count())
--- wardenclyffe/graphite/tests/test_models.py
+++ wardenclyffe/graphite/tests/test_models.py
@@ -1,7 +1,17 @@
 import unittest
 from wardenclyffe.graphite.models import operation_count_by_status
-
+from wardenclyffe.main.tests.factories import OperationFactory
 
 class TestCount(unittest.TestCase):
-    def test_base(self):
-        self.assertEqual(operation_count_by_status(), dict())
+    def test_empty(self):
+        self.assertEqual(
+            operation_count_by_status(),
+            dict(failed=0, complete=0, submitted=0, inprogress=0, enqueued=0))
+
+    def test_populated(self):
+        f = OperationFactory(status="failed")
+        self.assertEqual(operation_count_by_status()["failed"], 1)
+        self.assertEqual(operation_count_by_status()["enqueued"], 0)
+        self.assertEqual(operation_count_by_status()["submitted"], 0)
+        self.assertEqual(operation_count_by_status()["complete"], 0)
+
