0cef37011165e8ad87cdd016434a0c44a0f451f3
Anders Pearson <anders@columbia.edu>
2013-08-12 11:13:32 -0400

    submit as statsd gauges
    side by side for now. once i verify that gauges are working properly, i'll turn off the
    manual graphite reporting

--- wardenclyffe/graphite/tasks.py
+++ wardenclyffe/graphite/tasks.py
@@ -1,11 +1,15 @@
 from celery.decorators import periodic_task
 from celery.task.schedules import crontab
 from django.conf import settings
+from django_statsd.clients import statsd
 from socket import socket
 import sys
+from .models import operation_count_by_status
 from .models import operation_count_report
 from .models import tahoe_report
+from .models import tahoe_stats
 from .models import minutes_video_report
+from .models import minutes_video_stats
 
 
 def send_to_graphite(message):
@@ -27,13 +31,24 @@ def send_to_graphite(message):
 @periodic_task(run_every=crontab(hour="*", minute="*", day_of_week="*"))
 def operations_report():
     send_to_graphite(operation_count_report())
+    r = operation_count_by_status()
+    statsd.gauge("operations.failed", r['failed'])
+    statsd.gauge("operations.complete", r['complete'])
+    statsd.gauge("operations.submitted", r['submitted'])
+    statsd.gauge("operations.inprogress", r['in progress'])
+    statsd.gauge("operations.enqueued", r['enqueued'])
+    statsd.gauge("operations.total", sum(r.values()))
 
 
 @periodic_task(run_every=crontab(hour="*", minute="*", day_of_week="*"))
 def minutes_video():
     send_to_graphite(minutes_video_report())
+    statsd.gauge("minutes_video", minutes_video_stats())
 
 
 @periodic_task(run_every=crontab(hour="22", minute="13", day_of_week="*"))
 def nightly_tahoe_report():
     send_to_graphite(tahoe_report())
+    (cnt, total) = tahoe_stats()
+    statsd.gauge("tahoe.total", total)
+    statsd.gauge("tahoe.cnt", cnt)
