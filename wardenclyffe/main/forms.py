c512c8bbbaeddfda7c253f1bbf8149e9068c985f
Anders Pearson <anders@columbia.edu>
2015-08-21 14:08:02 +0200

    :arrow_up: Django 1.8

--- wardenclyffe/main/forms.py
+++ wardenclyffe/main/forms.py
@@ -5,18 +5,22 @@ from django import forms
 class AddCollectionForm(forms.ModelForm):
     class Meta:
         model = Collection
+        exclude = []
 
 
 class VideoForm(forms.ModelForm):
     class Meta:
         model = Video
+        exclude = []
 
 
 class ServerForm(forms.ModelForm):
     class Meta:
         model = Server
+        exclude = []
 
 
 class EditCollectionForm(forms.ModelForm):
     class Meta:
         model = Collection
+        exclude = []
