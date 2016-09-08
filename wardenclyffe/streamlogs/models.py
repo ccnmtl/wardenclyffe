24ca4cfd89ca9bd6ca49ece006792aee2da850e9
Anders Pearson <anders@columbia.edu>
2016-09-08 14:34:27 +0100

    define model for streamlogs

---
+++ wardenclyffe/streamlogs/models.py
@@ -0,0 +1,12 @@
+from django.db import models
+
+
+class StreamLog(models.Model):
+    filename = models.TextField(blank=True)
+    remote_addr = models.TextField(blank=True)
+    offset = models.TextField(blank=True)
+    referer = models.TextField(blank=True)
+    user_agent = models.TextField(blank=True)
+    # how they were authenticated: cookie, url hash, or public
+    access = models.TextField(blank=True)
+    request_at = models.DateTimeField(auto_now_add=True)
