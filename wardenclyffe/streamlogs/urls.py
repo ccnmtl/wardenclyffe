8963a940a906f736046a99106f4fe1e72db75b9b
Anders Pearson <anders@columbia.edu>
2016-09-15 15:19:48 +0100

    restrict access to staff

--- wardenclyffe/streamlogs/urls.py
+++ wardenclyffe/streamlogs/urls.py
@@ -1,17 +1,24 @@
 from django.conf.urls import url
+from django.contrib.auth.decorators import user_passes_test
 from django.views.generic.detail import DetailView
 from django.views.generic.list import ListView
+
+from wardenclyffe.main.views import is_staff
 
 from .models import StreamLog
 from .views import LogView
 
+staff_only = user_passes_test(lambda u: is_staff(u))
+
 urlpatterns = [
     url(r'^$', LogView.as_view(), name='streamlogs'),
-    url(r'list/$', ListView.as_view(
-        model=StreamLog,
-        paginate_by=50,
-    ), name='streamlogs-list'),
-    url(r'(?P<pk>\d+)/$', DetailView.as_view(
-        model=StreamLog,
-    ), name='streamlogs-detail'),
+    url(r'list/$', staff_only(
+        ListView.as_view(
+            model=StreamLog,
+            paginate_by=50,
+        )), name='streamlogs-list'),
+    url(r'(?P<pk>\d+)/$', staff_only(
+        DetailView.as_view(
+            model=StreamLog,
+        )), name='streamlogs-detail'),
 ]
