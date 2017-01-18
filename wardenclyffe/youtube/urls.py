7db5e318538abe6106e62dd705530126914f25e6
Anders Pearson <anders@columbia.edu>
2017-01-18 20:29:35 +0000

    first pass at youtube oauth flow

--- wardenclyffe/youtube/urls.py
+++ wardenclyffe/youtube/urls.py
@@ -6,4 +6,7 @@ urlpatterns = [
     url(r'^$', views.youtube, name='youtube-upload-form'),
     url(r'^post/$', views.youtube_post, name='youtube-post'),
     url(r'^done/$', views.youtube_done, name='youtube-done'),
+    url(r'auth/$', views.YTAuth.as_view(), name='youtube-auth'),
+    url(r'oauth2callback/$', views.OauthCallback.as_view(),
+        name='youtube-oauth2callback'),
 ]
