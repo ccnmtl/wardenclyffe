8b02f829f52a62806041a8e297cd50b3a20183af
Susan Dreher <sdreher@gmail.com>
2021-10-01 15:29:26 -0400

    Remove weekly s3 storage report
    The AWS management console does a very nice job at this. No need to add a graphite metric too.

--- wardenclyffe/graphite/tasks.py
+++ wardenclyffe/graphite/tasks.py
@@ -2,7 +2,7 @@ from celery.task.schedules import crontab
 from django_statsd.clients import statsd
 from wardenclyffe.celery import app
 from wardenclyffe.graphite.models import operation_count_by_status, \
-    minutes_video_stats, s3_stats
+    minutes_video_stats
 
 
 @app.task
@@ -23,14 +23,6 @@ def minutes_video():
     statsd.gauge("minutes_video", int(minutes_video_stats()))
 
 
-@app.task
-def weekly_s3_usage_report():
-    print("weekly_s3_report()")
-    (cnt, total) = s3_stats()
-    statsd.gauge("s3.total", total)
-    statsd.gauge("s3.cnt", cnt)
-
-
 @app.on_after_finalize.connect
 def setup_periodic_tasks(sender, **kwargs):
     sender.add_periodic_task(
@@ -40,7 +32,3 @@ def setup_periodic_tasks(sender, **kwargs):
     sender.add_periodic_task(
         crontab(hour='*', minute='*', day_of_week='*'),
         minutes_video.s())
-
-    sender.add_periodic_task(
-        crontab(hour=5, minute=0, day_of_week=0),
-        weekly_s3_usage_report.s())
