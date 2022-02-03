03eab305919c4404b21bdd09ebf817a01f9b556c
Susan Dreher <sdreher@gmail.com>
2022-02-03 13:00:41 -0500

    Fix reference to S3 bucket name

--- wardenclyffe/main/tasks.py
+++ wardenclyffe/main/tasks.py
@@ -285,7 +285,7 @@ def pull_from_s3(suffix, bucket_name, key):
     s3 = boto3.resource(
         's3', aws_access_key_id=settings.AWS_ACCESS_KEY,
         aws_secret_access_key=settings.AWS_SECRET_KEY)
-    bucket = s3.Bucket(settings.AWS_S3_UPLOAD_BUCKET)
+    bucket = s3.Bucket(bucket_name)
     k = bucket.Object(key)
 
     t = tempfile.NamedTemporaryFile(suffix=suffix)
