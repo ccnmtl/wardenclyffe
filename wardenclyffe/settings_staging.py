dec68a601d5f2ddc1f257df47e3a797342dec8c7
Nikolas Nyby <nnyby@columbia.edu>
2024-10-08 12:16:29 -0400

    Migrate to init_sentry

--- wardenclyffe/settings_staging.py
+++ wardenclyffe/settings_staging.py
@@ -1,9 +1,6 @@
-import sys
 from django.conf import settings
 from wardenclyffe.settings_shared import *  # noqa: F403
-from ctlsettings.staging import common
-import sentry_sdk
-from sentry_sdk.integrations.django import DjangoIntegration
+from ctlsettings.staging import common, init_sentry
 
 locals().update(
     common(
@@ -25,11 +22,5 @@ try:
 except ImportError:
     pass
 
-if ('migrate' not in sys.argv) and \
-   ('collectstatic' not in sys.argv) and \
-   hasattr(settings, 'SENTRY_DSN'):
-    sentry_sdk.init(
-        dsn=SENTRY_DSN,  # noqa: F405
-        integrations=[DjangoIntegration()],
-        debug=True,
-    )
+if hasattr(settings, 'SENTRY_DSN'):
+    init_sentry(SENTRY_DSN)  # noqa F405
