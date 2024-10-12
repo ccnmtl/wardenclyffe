543cf837c480c149572e9dd4d80237904a7dd889
Nikolas Nyby <nnyby@columbia.edu>
2024-10-11 22:23:23 -0400

    Update to new sentry-js setup

--- wardenclyffe/settings_shared.py
+++ wardenclyffe/settings_shared.py
@@ -162,6 +162,7 @@ TEMPLATES = [
                 'django.template.context_processors.request',
                 'django.contrib.messages.context_processors.messages',
                 'stagingcontext.staging_processor',
+                'ctlsettings.context_processors.env',
                 'gacontext.ga_processor',
                 'django.template.context_processors.csrf'
             ],
